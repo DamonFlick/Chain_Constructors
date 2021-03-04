6ad6204ecc0d00dc9711106635423be679e1555f
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-03-03 17:01:13 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain_Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string PlanetName = "Earth";
+            Console.WriteLine("Please name your Spaceship!");
+            var Name = Console.ReadLine();
+            Spaceship spaceship = new Spaceship(Name, PlanetName);
+
+            Console.WriteLine("Are you ready to depart Planet {0}?! \n Please answer 'yes' or 'no'", spaceship.Location);
+            if (Console.ReadLine() == "yes")
+            {
+                spaceship.BlastOff();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Spaceship.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain_Constructors
+{
+    public class Spaceship
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public string Location { get; set; }
+        public void BlastOff()
+        {
+            Console.WriteLine("You spaceship weighs {0} Lbs.", Weight);
+            Console.WriteLine("3... 2... 1...");
+            Console.ReadLine();
+            Console.WriteLine("Blast Off!!!");
+        }
+
+        public Spaceship(string name, string location, int weight)
+        {
+            Name = name;
+            Weight = weight;
+            Location = location;
+        }
+        public Spaceship(string name, string location) : this(name, location,  100)
+        {
+
+        }
+        
+    }    
+}
